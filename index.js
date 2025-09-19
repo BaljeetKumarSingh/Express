52957399d6246094239acedc73e664845d1953ec
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-09-19 11:30:14 +0530

    Specified Routes

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express"); // this return a function
-const path = require("path");
-
 const app = express();
 
 const port = 8080;
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./assets/img.jpg"));
-});
 
 app.listen(port, (error) => {
   if (!error)
@@ -15,4 +9,30 @@ app.listen(port, (error) => {
       `Server is Successfully Running, and App is listening on port ${port}`
     );
   else console.log(`error occurred, server can't start ${error}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("you contacted root path");
+});
+
+app.get("/search", (req, res) => {
+  res.send("you contacted search path");
+});
+
+app.get("/about", (req, res) => {
+  res.send("you contacted about path");
+});
+
+app.get("/home", (req, res) => {
+  res.send("you contacted home path");
+});
+
+// here we are creating post type response
+app.post("/", (req, res) => {
+  res.send("this is root post response!");
+});
+
+// custome response for paths other than the sepcified path
+app.use((req, res) => {
+  res.status(404).send("this path does not exist!");
 });
