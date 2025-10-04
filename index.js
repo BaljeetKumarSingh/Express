86108d9a777aa83005e6469bee88670f9fa59fbf
Baljeet Kumar <baljeetkumar2803@gmail.com>
2025-10-04 17:28:52 +0530

    Mimicing data visiualization from database json file

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const app = express();
 
 const port = 8080;
 
-//telling Express to use EJS as the view engine.
-// view engine -> is a special setting in Express that tells it which template engine to use.
-// ejs -> let you write HTML mixed with JS
-app.set("view engine", "ejs"); // this is aka templeting
-
-// if we try to run our server from parent folder of cwd it will throw an error : Failed to lookup view "home.ejs" in views directory "D:\Dev\Backend\views"
-// b/c express try for look for views in dir "D:\Dev\Backend\views"
-// to overcome this error we explicitly (hardcoded) define the path of our views dir.
-
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
@@ -21,9 +13,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/ig/:username", (req, res) => {
-  const followers = ["alice", "adam", "bob", "john", "Nikita"];
   let { username } = req.params;
-  res.render("instagram.ejs", { username, followers });
+  const instaData = require("./data.json"); // requiring a local file
+  const data = instaData[username]; // this data stores only username related data only
+  data
+    ? res.render("instagram.ejs", { data })
+    : res.render("error.ejs", { username });
 });
 
 app.get("/about", (req, res) => {
